345049f59c9c352b6b155ff998993f3fc54498fe
Danny Vazquez <dv5873@outlook.com>
2025-10-15 16:59:18 -0400

    Initiating as a UV project

---
+++ main.py
@@ -0,0 +1,9 @@
+def main():
+    print("Hello from mathcad-prime-xml-builder!")
+    import currentxlmParsingAndZip2
+
+    currentxlmParsingAndZip2.main()
+
+
+if __name__ == "__main__":
+    main()
