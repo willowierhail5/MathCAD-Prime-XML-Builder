83fb21d9ba2d1d8a32ce72fd1b21f2297fcf7600
Danny Vazquez <dv5873@outlook.com>
2025-10-19 11:57:56 -0400

    opening up test mathcad file automatically after script has ran

--- main.py
+++ main.py
@@ -11,6 +11,14 @@ def main():
 
 if __name__ == "__main__":
     main()
+    import subprocess
+
+    subprocess.run(
+        [
+            r"C:\Program Files\PTC\Mathcad Prime 11.0.1.0\MathcadPrime.exe",
+            r"C:\Users\Danny\source\repos\MathCAD-Prime-XML-Builder\mcdx\TestOutput2.mcdx",
+        ]
+    )
 
 # TODO: first, just look into programmatically adding a placeholder equation that is an input so we can modify it easily through the api.
 # TODO: then,
